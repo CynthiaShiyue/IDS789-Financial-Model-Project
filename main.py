1095d6b5d24a2209df51e8f3d3ae474cf5532fc9
周诗玥 <CynthiaSY@zhouzhihuadeMacBook-Pro.local>
2024-11-15 00:56:18 -0500

    cynthia:visualization

--- main.py
+++ main.py
@@ -3,29 +3,8 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from data_prepared.load import load
 from model.bayesian import bayesian_model
+from lib import msfe,plot_predictions_vs_actual
 
-def msfe(predictions, testing_dataset):
-    """
-    Calculate the Mean Squared Forecast Error (MSFE).
-    
-    Parameters:
-    predictions: Predicted values from the model.
-    testing_dataset: Actual values from the test dataset.
-    
-    Returns:
-    float: The Mean Squared Forecast Error.
-    """
-    # Ensure inputs are numpy arrays for element-wise operations
-    predictions = predictions
-    testing_dataset = testing_dataset
-    
-    # Calculate squared errors
-    squared_errors = (predictions - testing_dataset) ** 2
-    
-    # Calculate the mean of squared errors
-    msfe_value = squared_errors.mean()
-    
-    return msfe_value
 
 def main():
     training_dataset, testing_dataset = load()
@@ -36,9 +15,10 @@ def main():
     
     
     
-    
     # calculate error
-    error_bayesian=msfe(bayesian_predictions, testing_dataset)
+    #error_bayesian=msfe(bayesian_predictions, testing_realY)
+    
+    
     
     # comparing error
     
