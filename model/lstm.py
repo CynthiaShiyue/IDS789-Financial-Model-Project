9925962f5608bc7dd2646f46e1e02278b8b0ea02
monatagelsir7 <monatagelsir777@gmail.com>
2024-12-09 23:21:10 -0500

    changing colors

--- model/lstm.py
+++ model/lstm.py
@@ -7,13 +7,25 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 def lstm(data_train, data_test):
 
     # data_train = pd.read_csv("data_prepared/training_dataset.csv")
 
-    features = ['UBS log_return', 'Bid-Ask Spread', 'volume', 'VIX', 'EURCHF', 'oil_log_return', 'gold_log_return', 
-                'DB log_return', 'MS log_return', 'SPY log_return', '^FTSE log_return']
-    target = 'UBS log_return'
+    features = [
+        "UBS log_return",
+        "Bid-Ask Spread",
+        "volume",
+        "VIX",
+        "EURCHF",
+        "oil_log_return",
+        "gold_log_return",
+        "DB log_return",
+        "MS log_return",
+        "SPY log_return",
+        "^FTSE log_return",
+    ]
+    target = "UBS log_return"
 
     X_train = data_train[features].values
     y_train = data_train[target].values
@@ -21,19 +33,27 @@ def lstm(data_train, data_test):
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
 
-    X_train_scaled = np.reshape(X_train_scaled, (X_train_scaled.shape[0], 1, X_train_scaled.shape[1]))
+    X_train_scaled = np.reshape(
+        X_train_scaled, (X_train_scaled.shape[0], 1, X_train_scaled.shape[1])
+    )
 
     # Note: the default tanh activation works best compared to the other tested functions: sigmoid, relu
-    model = Sequential([
-        LSTM(units=128, input_shape=(X_train_scaled.shape[1], X_train_scaled.shape[2]), return_sequences=True),
-        Dropout(0.2),
-        LSTM(units=64, return_sequences=False),
-        Dropout(0.2),
-        Dense(32),
-        Dense(1)
-    ])
+    model = Sequential(
+        [
+            LSTM(
+                units=128,
+                input_shape=(X_train_scaled.shape[1], X_train_scaled.shape[2]),
+                return_sequences=True,
+            ),
+            Dropout(0.2),
+            LSTM(units=64, return_sequences=False),
+            Dropout(0.2),
+            Dense(32),
+            Dense(1),
+        ]
+    )
 
-    model.compile(optimizer='adam', loss='mse', metrics=['mae'])
+    model.compile(optimizer="adam", loss="mse", metrics=["mae"])
 
     model.fit(X_train_scaled, y_train, epochs=50, batch_size=32, verbose=1)
 
@@ -41,9 +61,10 @@ def lstm(data_train, data_test):
 
     X_test = data_test[features].values
     X_test_scaled = scaler.transform(X_test)
-    X_test_scaled = np.reshape(X_test_scaled, (X_test_scaled.shape[0], 1, X_test_scaled.shape[1]))
+    X_test_scaled = np.reshape(
+        X_test_scaled, (X_test_scaled.shape[0], 1, X_test_scaled.shape[1])
+    )
 
     y_pred = model.predict(X_test_scaled)
 
     return y_pred
-
